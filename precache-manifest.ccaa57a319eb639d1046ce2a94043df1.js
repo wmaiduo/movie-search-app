5099dc3a0d93a1fbd674d14ccf4874028ec60be4
wmaiduo <wmaiduo@gmail.com>
2020-07-29 12:45:12 -0700

    Updates

---
+++ precache-manifest.ccaa57a319eb639d1046ce2a94043df1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84111c662c5a7ad95e22822f7ff08c3b",
+    "url": "/movie-search-app/index.html"
+  },
+  {
+    "revision": "412e5888e7ce40f79d96",
+    "url": "/movie-search-app/static/css/main.ed02f10a.chunk.css"
+  },
+  {
+    "revision": "589bb80cf9f1c2a31938",
+    "url": "/movie-search-app/static/js/2.13e8c1b4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie-search-app/static/js/2.13e8c1b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "412e5888e7ce40f79d96",
+    "url": "/movie-search-app/static/js/main.00a9ef3a.chunk.js"
+  },
+  {
+    "revision": "24d503fa810a63e0a34a",
+    "url": "/movie-search-app/static/js/runtime-main.238d2597.js"
+  }
+]);
